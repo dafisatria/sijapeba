b1a794e3bc849a4de97cd5c65b420056eea6d379
dafisatria <satria1002@gmail.com>
2022-06-19 17:08:20 +0700

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,45 @@
+@include('layout.header')
+<div class="site-blocks-cover inner-page-cover overlay" style="background-image: url(images/hero_bg_1.jpg);"
+    data-aos="fade" data-stellar-background-ratio="0.5">
+    <div class="container">
+        <div class="row align-items-center justify-content-center text-center">
+
+            <div class="col-md-8" data-aos="fade-up" data-aos-delay="400">
+                <h1 class="text-white font-weight-light text-uppercase font-weight-bold">Hubungi Kami</h1>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="site-section bg-light">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+
+                <div class="p-4 mb-3 bg-white">
+                    <p class="mb-0 font-weight-bold">Alamat</p>
+                    <p class="mb-4">Jl. Soekarno Hatta no.11</p>
+
+                    <p class="mb-0 font-weight-bold">Telepon</p>
+                    <p class="mb-4"><a href="#">+62 822 643 793 68</a></p>
+
+                    <p class="mb-0 font-weight-bold">E-mail</p>
+                    <p class="mb-0"><a href="#">didit@gmail.com</a></p>
+
+                </div>
+
+                <div class="p-4 mb-3 bg-white">
+                    <h3 class="h5 text-black mb-3">Info Lebih Lanjut</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa ad iure porro mollitia architecto
+                        hic consequuntur. Distinctio nisi perferendis dolore, ipsa consectetur? Fugiat quaerat eos qui,
+                        libero neque sed nulla.</p>
+                    <p><a href="#" class="btn btn-primary px-4 py-2 text-white">Learn More</a></p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+@include('layout.footer')
---
+++ resources/views/index.blade.php
@@ -0,0 +1,94 @@
+@include('layout.header')
+<div class="site-blocks-cover overlay" style="background-image: url(images/hero_bg_1.jpg);" data-aos="fade"
+    data-stellar-background-ratio="0.5">
+    <div class="container">
+        <div class="row align-items-center justify-content-center text-center">
+
+            <div class="col-md-8" data-aos="fade-up" data-aos-delay="400">
+
+
+                <h1 class="text-white font-weight-light mb-5 text-uppercase font-weight-bold">Layanan Pengiriman Seluruh
+                    Indonesia</h1>
+                <p><a href="#" class="btn btn-primary py-3 px-5 text-white">Cek Resimu disini!</a></p>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="site-section">
+    <div class="container">
+        <div class="row justify-content-center mb-5">
+            <div class="col-md-7 text-center border-primary">
+                <h2 class="mb-0 text-primary">Pelayanan yang Kami Tawarkan</h2>
+                <p class="color-black-opacity-5">Sistem Informasi Jasa Pengiriman Barang</p>
+            </div>
+        </div>
+        <div class="row align-items-stretch">
+            <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
+                <div class="unit-4 d-flex">
+                    <div class="unit-4-icon mr-4"><span class="text-primary flaticon-travel"></span></div>
+                    <div>
+                        <h3>Pengiriman Udara</h3>
+                        <p>Mengirim barang kirimanmu sampai dengan cepat melalui awak pesawat.</p>
+                        <p class="mb-0"><a href="#">Learn More</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
+                <div class="unit-4 d-flex">
+                    <div class="unit-4-icon mr-4"><span class="text-primary flaticon-sea-ship-with-containers"></span>
+                    </div>
+                    <div>
+                        <h3>Pengiriman Laut</h3>
+                        <p>Siap mengirim paket hingga pulau terpelosok dengan adanya pengiriman laut yang diangkut
+                            dengan kapal pengiriman.</p>
+                        <p class="mb-0"><a href="#">Learn More</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
+                <div class="unit-4 d-flex">
+                    <div class="unit-4-icon mr-4"><span class="text-primary flaticon-frontal-truck"></span></div>
+                    <div>
+                        <h3>Pengiriman Darat</h3>
+                        <p>Mengirim paket kiriman antar atau bahkan dalam kota dengan pengiriman darat.</p>
+                        <p class="mb-0"><a href="#">Learn More</a></p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
+
+<div class="site-blocks-cover overlay inner-page-cover"
+    style="background-image: url(images/hero_bg_2.jpg); background-attachment: fixed;">
+    <div class="container">
+        <div class="row align-items-center justify-content-center text-center">
+
+            <div class="col-md-7" data-aos="fade-up" data-aos-delay="400">
+                <a href="https://vimeo.com/channels/staffpicks/93951774"
+                    class="play-single-big mb-4 d-inline-block popup-vimeo"><span class="icon-play"></span></a>
+                <h2 class="text-white font-weight-light mb-5 h1">View Our Services By Watching This Short Video</h2>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="site-section border-top">
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-md-12">
+                <h2 class="mb-5 text-black">Bergabung dengan Kami</h2>
+                <p class="mb-0"><a href="booking.html" class="btn btn-primary py-3 px-5 text-white">Get Started</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+@include('layout.footer')
